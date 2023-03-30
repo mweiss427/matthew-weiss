744ad8cb1f396718ea0ff032e78325002f63e7f9
Matt Weiss <mweiss@northwindstech.com>
2023-03-30 08:26:59 -0500

    Moving unwanted h1 block

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,6 @@ import Footer from "../components/Footer";
 const HomePage = () => {
   return (
     <>
-      <h1>Welcome to Matthew E Weiss' site</h1>
-      {/* Add any other content you want on your home page */}
       <Footer
         title="Thank you"
         description="I hope you found value in my site.  Thank you for your time."
